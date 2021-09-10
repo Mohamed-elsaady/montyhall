e59c4edfa55ffeea3a85a2ac05a75ba907f7182c
Mohamed Mahmoud Gamal Eldin Elsaady <melsaady@asu.edu>
2021-09-11 02:09:54 +0400

    Monty Hall Problem Game Related Functions, Stimulations And Outcomes.
    initial commit
    
    Co-Authored-By: Jesse Lecy <1209099+lecy@users.noreply.github.com>

---
+++ R/monty-hall-problem.R
@@ -0,0 +1,243 @@
+#' @title
+#'   Create a new Monty Hall Problem game.
+#'
+#' @description
+#'   `create_game()` generates a new game that consists of two doors 
+#'   with goats behind them, and one with a car.
+#'
+#' @details
+#'   The game setup replicates the game on the TV show "Let's
+#'   Make a Deal" where there are three doors for a contestant
+#'   to choose from, one of which has a car behind it and two 
+#'   have goats. The contestant selects a door, then the host
+#'   opens a door to reveal a goat, and then the contestant is
+#'   given an opportunity to stay with their original selection
+#'   or switch to the other unopened door. There was a famous 
+#'   debate about whether it was optimal to stay or switch when
+#'   given the option to switch, so this simulation was created
+#'   to test both strategies. 
+#'
+#' @param ... the function uses no arguments.
+#' 
+#' @return 
+#' The function returns a length 3 character vector
+#'   indicating the positions of goats and the car.
+#'
+#' @examples
+#'   create_game()
+#'
+#' @export
+  create_game <- function()
+  {
+      a.game <- sample( x=c("goat","goat","car"), size=3, replace=F )
+      return( a.game )
+  } 
+  
+
+
+
+#' @title 
+#'   Door selection function by the contestant.
+#' 
+#' @description
+#'   `select_door()` The contestant selects one door from 3 selections.
+#' 
+#' @details
+#'   This function selects a random door from 3 selections.
+#' 
+#' @param  ... the function uses no arguments.
+#' 
+#' @return  
+#' Numeric value between 1 and 3. 
+#' 
+#' @examples select_door() 
+#' 
+#' @export
+  select_door <- function( )
+  {
+    doors <- c(1,2,3) 
+    a.pick <- sample( doors, size=1 )
+    return( a.pick )  # number between 1 and 3
+  }
+
+
+
+#' @title
+#'   Door opening function by the host 
+#'   
+#' @description
+#'   `open_goat_door()` The host chooses a door with a goat behind it, 
+#'   which can't be a door the contestant had already selected.
+#' 
+#' @param a.pick Numeric value.
+#' 
+#' @param game length 3 character vector indicating the positions of goats and the car.
+#' 
+#' @return  
+#' Numeric value between 1 and 3.
+#' 
+#' @examples open_goat_door()
+#' 
+#' @export
+  open_goat_door <- function( game, a.pick )
+  {
+     doors <- c(1,2,3)
+     # if contestant selected car,
+     # randomly select one of two goats 
+     if( game[ a.pick ] == "car" )
+     { 
+       goat.doors <- doors[ game != "car" ] 
+       opened.door <- sample( goat.doors, size=1 )
+     }
+     if( game[ a.pick ] == "goat" )
+     { 
+       opened.door <- doors[ game != "car" & doors != a.pick ] 
+     }
+     return( opened.door ) # number between 1 and 3
+  }
+
+
+#' @title 
+#'   Door changing function by the contestant 
+#'   
+#' @description
+#'    `change_door()` The contestant can switch his door selection to another closed door or 
+#'    stay with his current door selection.
+#'    
+#' @param stay  A logical parameter represents the game-playing strategy. 
+#' True for the contestant switching his door selection to another closed door.
+#' False for stay with his current door selection.
+#' Default is **True**.
+#' 
+#' @param opened.door  Numeric value.
+#' 
+#' @param a.pick  Numeric value. 
+#' 
+#' @return 
+#' Numeric value between 1 and 3.
+#' 
+#' @examples change_door(stay=T, opened.door, a.pick )
+#' 
+#' @export
+  change_door <- function( stay=T, opened.door, a.pick )
+  {
+     doors <- c(1,2,3) 
+     
+     if( stay )
+     {
+       final.pick <- a.pick
+     }
+     if( ! stay )
+     {
+       final.pick <- doors[ doors != opened.door & doors != a.pick ] 
+     }
+    
+     return( final.pick )  # number between 1 and 3
+  }
+
+
+
+#' @title
+#'  Function to check if the contestant has won.
+#'  
+#' @description
+#'  `determine_winner()` Revealing function to check if the contestant has won or lost.
+#'  
+#' @param final.pick  Numeric value.
+#' 
+#' @param game length 3 character vector indicating the positions of goats and the car.
+#' 
+#' @return 
+#' a string  of either **"WIN"** in case the contestant has won  or **"LOSE"** in case the contestant has lost.
+#' 
+#' @examples determine_winner(final.pick, game)
+#' 
+#' @export
+  determine_winner <- function( final.pick, game )
+  {
+     if( game[ final.pick ] == "car" )
+     {
+        return( "WIN" )
+     }
+     if( game[ final.pick ] == "goat" )
+     {
+        return( "LOSE" )
+     }
+  }
+  
+
+
+
+
+#' @title
+#' Function to encapsulate all the Monty Hall Problem game functions into one function.
+#' 
+#' @description
+#' `play_game()` encapsulates `create_game()` , `select_door()` , `open_goat_door()` , `change_door()` , `determine_winner()` to determine the game results.
+#' 
+#' @param ... the function uses no arguments.
+#' 
+#' @return 
+#' data.frame containing the strategy of staying or switching and the outcome.
+#' 
+#' @examples play_game()
+#' 
+#' @export
+  play_game <- function( )
+  {
+    new.game <- create_game()
+    first.pick <- select_door()
+    opened.door <- open_goat_door( new.game, first.pick )
+  
+    final.pick.stay <- change_door( stay=T, opened.door, first.pick )
+    final.pick.switch <- change_door( stay=F, opened.door, first.pick )
+  
+    outcome.stay <- determine_winner( final.pick.stay, new.game  )
+    outcome.switch <- determine_winner( final.pick.switch, new.game )
+    
+    strategy <- c("stay","switch")
+    outcome <- c(outcome.stay,outcome.switch)
+    game.results <- data.frame( strategy, outcome,
+                                stringsAsFactors=F )
+    return( game.results )
+  }
+
+
+#' @title
+#' Function to stimulate the Monty Hall Problem game.
+#' 
+#' @description
+#' `play_n_games()` Runs stimulation of the Monty Hall Problem game and return the results as a list.
+#' 
+#' @param n is the number of stimulations.
+#' 
+#' @return 
+#' a list of the results
+#' 
+#' @examples play_n_games(n=100)
+#' 
+#' @export
+  play_n_games <- function( n=100 )
+  {
+    
+    library( dplyr )
+    results.list <- list()   # collector
+    loop.count <- 1
+  
+    for( i in 1:n )  # iterator
+    {
+      game.outcome <- play_game()
+      results.list[[ loop.count ]] <- game.outcome 
+      loop.count <- loop.count + 1
+    }
+    
+    results.df <- dplyr::bind_rows( results.list )
+  
+    table( results.df ) %>% 
+    prop.table( margin=1 ) %>%  # row proportions
+    round( 2 ) %>% 
+    print()
+    
+    return( results.df )
+  
+  }
